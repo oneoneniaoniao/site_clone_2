b86f2f8b68971d93acd6ba7fa8aa46831d5d9696
oneoneniaoniao <321marumaru123@gmail.com>
2021-08-26 22:12:47 +0900

    Add CSS for SP main contents close #6

--- header.js
+++ header.js
@@ -10,3 +10,13 @@ $(function () {
   });
 });
 
+$(function(){
+  $('#nav-content .category li a').each(function(){
+      var $href = $(this).attr('href');
+      if(location.href.match($href)) {
+      $(this).addClass('active');
+      } else {
+      $(this).removeClass('active');
+      }
+  });
+});
